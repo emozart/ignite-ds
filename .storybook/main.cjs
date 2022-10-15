e7d2527dc675de1ab1ba46f50e0ec3b038de600e
Elton Mozart <elton.silva.ext@dasa.com.br>
2022-10-15 20:01:49 -0300

    :bricks: ci: Fixed folder name

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,7 +14,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
-      config.base = "/ignitelab-ds/";
+      config.base = "/ignite-ds/";
     }
     return config;
   },
