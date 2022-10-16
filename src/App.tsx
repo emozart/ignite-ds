2f43ca92ebd0843a6ae1d13282670d94f362e14c
Elton Mozart <elton.silva.ext@dasa.com.br>
2022-10-16 00:58:08 -0300

    :construction: Update App with components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,68 @@
+import { Envelope } from "phosphor-react";
+import { Lock } from "phosphor-react";
+import { Button } from "./components/Button";
+import { Checkbox } from "./components/Checkbox";
+import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
+import { Text } from "./components/Text";
+import { TextInput } from "./components/TextInput";
 import "./styles/global.css";
 
 export function App() {
   return (
-    <>
-      <h1 className="font-bold text-2xl text-gray-400">Hello world</h1>
-      <button className="bg-cyan-500 hover:bg-cyan-300 text-white font-bold py-2 px-4 rounded">
-        Enviar
-      </button>
-    </>
+    <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
+      <header className="flex flex-col items-center">
+        <Logo />
+        <Heading size="lg" className="mt-4 ">
+          Ignite Lab
+        </Heading>
+        <Text size="sm" className="text-gray-400 mt-2">
+          Faça login e comece a usar
+        </Text>
+      </header>
+      <form className="flex flex-col itens-stretch gap-4 w-full max-w-sm mt-10">
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text className="font-semibold">Endereço de e-mail</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input id="email" placeholder="Digite seu e-mail" />
+          </TextInput.Root>
+        </label>
+        <label htmlFor="password" className="flex flex-col gap-3">
+          <Text className="font-semibold">Sua senha</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input
+              type="password"
+              id="password"
+              placeholder="********"
+            />
+          </TextInput.Root>
+        </label>
+        <label htmlFor="remember" className="flex items-center gap-2">
+          <Checkbox id="remember" />
+          <Text size="sm">Lembrar-me por 30 dias</Text>
+        </label>
+        <Button type="submit" className="mt-4">
+          Entrar na plataforma
+        </Button>
+      </form>
+      <footer className="flex flex-col items-center gap-4 mt-8">
+        <Text size="sm" asChild>
+          <a href="#" className="text-gray-400 hover:text-gray-200 underline">
+            Esqueceu sua senha?
+          </a>
+        </Text>
+        <Text size="sm" asChild>
+          <a href="#" className="text-gray-400 hover:text-gray-200 underline">
+            Não possui conta? Crie uma agora.
+          </a>
+        </Text>
+      </footer>
+    </div>
   );
 }
